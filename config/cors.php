d2f01a2819347784b5d2f776d7fbc49430d4f491
867378 <ongvincentdavid@gmail.com>
2025-06-07 19:13:35 +0800

    test connection

--- config/cors.php
+++ config/cors.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Cross-Origin Resource Sharing (CORS) Configuration
@@ -19,7 +18,9 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173','http://localhost:5174','http://localhost:8080'],
+    'allowed_origins' => [
+        'gcch-frontend.vercel.app',
+    ],
 
     'allowed_origins_patterns' => ['*'],
 
